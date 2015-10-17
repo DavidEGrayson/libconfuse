51090b4b4ccd7a1f6661f8644705701bbc9cccb3
David Grayson <davidegrayson@gmail.com>
2015-10-17 14:49:40 -0700

    Attempt

--- appveyor/msys2-prepare.ps1
+++ appveyor/msys2-prepare.ps1
@@ -1,7 +1,7 @@
 cd C:/msys64
 $env:MSYSTEM = "MSYS"
 usr\bin\bash -l -c "true"
-usb\bin\bash -l -c "update-core"
+usr\bin\bash -l -c "update-core"
 usb\bin\bash -l -c "pacman --noconfirm -Su"
 usr\bin\bash -l -c "pacman --noconfirm -S mingw-w64-x86_64-gcc autoconf flex make libtool"
 usr\bin\bash -l -c "env"
