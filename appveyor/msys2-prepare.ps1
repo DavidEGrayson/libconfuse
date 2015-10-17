c7779953f3ce5788799a0b4da745621a22d359aa
David Grayson <davidegrayson@gmail.com>
2015-10-17 14:59:18 -0700

    attempt

--- appveyor/msys2-prepare.ps1
+++ appveyor/msys2-prepare.ps1
@@ -1,8 +1,8 @@
 cd C:/msys64
 $env:MSYSTEM = "MSYS"
 usr\bin\bash -l -c "true"
-usb\bin\bash -l -c "pacman -Q"
+usr\bin\bash -l -c "pacman -Q"
 # usr\bin\bash -l -c "update-core"
 # usr\bin\bash -l -c "pacman --noconfirm -Su"
-usr\bin\bash -l -c "pacman --noconfirm -S mingw-w64-x86_64-gcc autoconf flex make libtool"
+usr\bin\bash -l -c "pacman --noconfirm --needed -S mingw-w64-x86_64-gcc autoconf flex make libtool"
 usr\bin\bash -l -c "env"
