445976ef7083d160e3d7e8475ca947d9a4b889f2
David Grayson <davidegrayson@gmail.com>
2015-10-17 14:57:54 -0700

    Don't update so much stuff.

--- appveyor/msys2-prepare.ps1
+++ appveyor/msys2-prepare.ps1
@@ -1,7 +1,8 @@
 cd C:/msys64
 $env:MSYSTEM = "MSYS"
 usr\bin\bash -l -c "true"
-usr\bin\bash -l -c "update-core"
-usr\bin\bash -l -c "pacman --noconfirm -Su"
+usb\bin\bash -l -c "pacman -Q"
+# usr\bin\bash -l -c "update-core"
+# usr\bin\bash -l -c "pacman --noconfirm -Su"
 usr\bin\bash -l -c "pacman --noconfirm -S mingw-w64-x86_64-gcc autoconf flex make libtool"
 usr\bin\bash -l -c "env"
