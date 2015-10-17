626edf116489ba6d0ac2ffed885281b5a1c7dc74
David Grayson <davidegrayson@gmail.com>
2015-10-17 15:23:03 -0700

    simplify the ps1

--- appveyor/msys2-build.ps1
+++ appveyor/msys2-build.ps1
@@ -1,9 +1,10 @@
 $env:MSYSTEM = "MINGW64"
 $env:HOME = "C:/projects/libconfuse"
-C:/msys64/usr/bin/bash -l -c "./autogen.sh"
 C:/msys64/usr/bin/bash -l -c @"
+./autogen.sh
 ./configure --prefix=/mingw64 \
   --build=x86_64-w64-mingw32 --host=x86_64-w64-mingw32 \
   --enable-static --enable-shared --disable-examples
+make check
 "@
-C:/msys64/usr/bin/bash -l -c "make check"
+
