6979b3c59c0a53f224b7fe80bb71c97a2a1788f4
David Grayson <davidegrayson@gmail.com>
2015-10-17 15:43:26 -0700

    try

--- appveyor/msys2-build.ps1
+++ appveyor/msys2-build.ps1
@@ -1,6 +1,6 @@
 $env:MSYSTEM = "MINGW64"
 C:/msys64/usr/bin/bash -l -c @"
-cd $pwd
+cd $($pwd | % {$_ -replace '\\', '/'})
 ./autogen.sh
 ./configure --prefix=/mingw64 \
   --build=x86_64-w64-mingw32 --host=x86_64-w64-mingw32 \
