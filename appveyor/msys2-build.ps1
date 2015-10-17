f28b10892e203833fba86e9e52d9ce850823c0d9
David Grayson <davidegrayson@gmail.com>
2015-10-17 16:11:42 -0700

    Try to get 32-bit in appveyor

--- appveyor/msys2-build.ps1
+++ appveyor/msys2-build.ps1
@@ -1,10 +1,26 @@
-$env:MSYSTEM = "MINGW64"
-C:/msys64/usr/bin/bash -l -c @"
-cd $($pwd | % {$_ -replace '\\', '/'})
+$prepare = @"
 ./autogen.sh
-./configure --prefix=/mingw64 \
-  --build=x86_64-w64-mingw32 --host=x86_64-w64-mingw32 \
+"@
+
+$build = @"
+mkdir build-`$MINGW_CHOST
+cd build-`$MINGW_CHOST
+../configure --prefix=`$MINGW_PREFIX \
+  --build=`$MINGW_CHOST --host=`$MINGW_CHOST \
   --enable-static --enable-shared --disable-examples
 make check
 "@
 
+$env:CHERE_INVOKING = "1"
+$env:MSYSTEM = "MSYS2"
+D:/msys64/usr/bin/bash -l -c $prepare
+
+$env:MSYSTEM = "MINGW64"
+$env:MINGW_PREFIX = "/mingw64"
+$env:MINGW_CHOST = "x86_64-w64-mingw32"
+D:/msys64/usr/bin/bash -l -c $build
+
+$env:MSYSTEM = "MINGW32"
+$env:MINGW_PREFIX = "/mingw32"
+$env:MINGW_CHOST = "i686-w64-mingw32"
+D:/msys64/usr/bin/bash -l -c $build
--- appveyor/msys2-prepare.ps1
+++
@@ -1,6 +0,0 @@
-cd C:/msys64
-$env:MSYSTEM = "MSYS"
-usr\bin\bash -l -c "true"
-usr\bin\bash -l -c "pacman -Q"
-# usr\bin\bash -l -c "pacman --noconfirm --needed -q -S mingw-w64-x86_64-gcc autoconf flex make libtool"
-
