e0ce81ff8cf12effcfaa9c0178d8b7f61b24fb3d
David Grayson <davidegrayson@gmail.com>
2015-10-17 15:29:26 -0700

    Don't lie about HOME'

--- appveyor/msys2-build.ps1
+++ appveyor/msys2-build.ps1
@@ -1,6 +1,6 @@
 $env:MSYSTEM = "MINGW64"
-$env:HOME = "C:/projects/libconfuse"
 C:/msys64/usr/bin/bash -l -c @"
+cd /c/projects/libconfuse
 ./autogen.sh
 ./configure --prefix=/mingw64 \
   --build=x86_64-w64-mingw32 --host=x86_64-w64-mingw32 \
