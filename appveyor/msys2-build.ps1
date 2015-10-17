06c021887b72438023b19544d2f09a802fbeecd6
David Grayson <davidegrayson@gmail.com>
2015-10-17 15:08:48 -0700

    Attempt

--- appveyor/msys2-build.ps1
+++ appveyor/msys2-build.ps1
@@ -1,4 +1,6 @@
 $env:MSYSTEM = "MINGW64"
+C:/msys64/usr/bin/bash -l -c "pwd"
+C:/msys64/usr/bin/bash -l -c "ls"
 C:/msys64/usr/bin/bash -l -c "./autogen.sh"
 C:/msys64/usr/bin/bash -l -c "./configure --prefix=/mingw64 --build=x86_64-w64-mingw32 --host=x86_64-w64-mingw32 --enable-static --enable-shared --disable-examples"
 C:/msys64/usr/bin/bash -l -c "make check"
