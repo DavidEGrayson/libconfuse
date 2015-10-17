bcc4daa3b634259a6bd858cd526b7a232bab006f
David Grayson <davidegrayson@gmail.com>
2015-10-17 16:17:40 -0700

    try

--- appveyor/msys2-build.ps1
+++ appveyor/msys2-build.ps1
@@ -12,15 +12,15 @@ make check
 "@
 
 $env:CHERE_INVOKING = "1"
-$env:MSYSTEM = "MSYS2"
-C:/msys64/usr/bin/bash -l -c $prepare
+$env:MSYSTEM = "MINGW64"
+C:/msys64/usr/bin/bash -e -l -c $prepare
 
 $env:MSYSTEM = "MINGW64"
 $env:MINGW_PREFIX = "/mingw64"
 $env:MINGW_CHOST = "x86_64-w64-mingw32"
-C:/msys64/usr/bin/bash -l -c $build
+C:/msys64/usr/bin/bash -e -l -c $build
 
 $env:MSYSTEM = "MINGW32"
 $env:MINGW_PREFIX = "/mingw32"
 $env:MINGW_CHOST = "i686-w64-mingw32"
-C:/msys64/usr/bin/bash -l -c $build
+C:/msys64/usr/bin/bash -e -l -c $build
