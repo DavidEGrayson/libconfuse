6a21c654b05d0607f0c0e69d55d741ef23b6da83
David Grayson <davidegrayson@gmail.com>
2015-10-17 14:46:02 -0700

    print env

--- appveyor/msys2-prepare.ps1
+++ appveyor/msys2-prepare.ps1
@@ -4,5 +4,4 @@ usr\bin\bash -l -c "true"
 usb\bin\bash -l -c "update-core"
 usb\bin\bash -l -c "pacman -Su"
 usr\bin\bash -l -c "pacman -S mingw-w64-x86_64-gcc autoconf flex make libtool"
-
-
+usr\bin\bash -l -c "env"
